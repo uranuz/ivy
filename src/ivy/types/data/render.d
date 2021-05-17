de8c45ac2b2b531a47b508036131b8f2d82b641b
uranuz <neuranuz@gmail.com>
2021-05-17 21:16:10 +0300

    Implemented ijection of opts into markup rendering pipline

--- src/ivy/types/data/render.d
+++ src/ivy/types/data/render.d
@@ -137,12 +137,19 @@ void renderDataNode(DataRenderType renderType, IvyData, OutRange, Interp...)(
 					DataRenderType.HTML,
 					DataRenderType.HTMLDebug,
 				].canFind(renderType);
+
 				static if( useRender ) {
-					enum outMethod = "render";
+					import std.traits: isInstanceOf, TemplateOf;
+					static if( isInstanceOf!(FirControlRender, OutRange) ) {
+						auto firRender = FirControlRender!(OutRange.OutRange, typeof(interp[0]))(*sink._outRange, classNode, interp[0]);
+					} else {
+						auto firRender = FirControlRender!(OutRange, typeof(interp[0]))(sink, classNode, interp[0]);
+					}
+					renderDataNode!renderType(firRender, interp[0].execClassMethodSync(classNode, "render"), interp);
 				} else {
-					enum outMethod = "__serialize__";
+					renderDataNode!renderType(sink, interp[0].execClassMethodSync(classNode, "__serialize__"), interp);
 				}
-				renderDataNode!renderType(sink, interp[0].execClassMethodSync(classNode, outMethod), interp);
+				
 			} else {
 				_writeStr!renderType(sink, "[[class node]]");
 			}
@@ -179,4 +186,104 @@ string renderDataNode2(DataRenderType renderType, IvyData, Interp...)(
 	auto result = appender!string();
 	renderDataNode!renderType(result, node, interp);
 	return result.data;
+}
+
+struct FirControlRender(O, Interp)
+{
+	import std.range: empty;
+	import trifle.parse_utils: front, popFront;
+
+	import ivy.types.data.iface.class_node: IClassNode;
+
+	alias OutRange = O;
+
+	OutRange* _outRange;
+	IClassNode _classNode;
+	Interp _interp;
+
+	ParseState _state;
+	bool _isQuotedAttr = false;
+
+	enum ParseState: ubyte {
+		init,
+		tagOpened,
+		tagClosed
+	}
+
+
+	this(ref OutRange outRange, IClassNode classNode, Interp interp)
+	{
+		this._outRange = &outRange;
+		this._classNode = classNode;
+		this._interp = interp;
+	}
+
+	void put(string val)
+	{
+		if (this._state == ParseState.tagClosed) {
+			// If tag is closed then just put rest data to out range
+			(*this._outRange).put(val);
+			return;
+		}
+		
+		while( !val.empty )
+		{
+			this.put(val.front);
+			// Move to the next character
+			val.popFront();
+		}
+	}
+
+	void put(char ch)
+	{
+		import std.conv: to;
+
+		this._handleChar(ch);
+		// Put character into output
+		(*this._outRange).put(ch.to!string);
+	}
+
+
+	void _handleChar(char ch)
+	{
+		if( ch == '"' )
+		{
+			// Handle quotes around tag attributes and toggle "quoted" mode
+			this._isQuotedAttr = !this._isQuotedAttr;
+			return;
+		}
+		if( this._isQuotedAttr ) {
+			// If we are in quoted mode then don't analyze tag open/ close...
+			return;
+		}
+		if( ch == '<' && this._state == ParseState.init ) {
+			// If we are in init state then detect that first tag was opened...
+			this._state = ParseState.tagOpened;
+			return;
+		}
+		if( ch == '>' && this._state == ParseState.tagOpened )
+		{
+			// If first tag was opened then detect that it was closed
+			this._state = ParseState.tagClosed;
+			// Inject extra data before closing tag
+			this._injectExtras();
+		}
+	}
+
+	void _injectExtras()
+	{
+		import ivy.types.data: IvyData;
+
+		import std.base64: Base64;
+
+		string moduleName = this._classNode.__getAttr__("moduleName").str;
+		string encodedOpts = cast(string) Base64.encode(
+			cast(ubyte[]) renderDataNode2!(DataRenderType.JSON)(IvyData(this._classNode), this._interp));
+
+		(*this._outRange).put(" data-fir-module=\"");
+		(*this._outRange).put(moduleName);
+		(*this._outRange).put("\" data-fir-opts=\"");
+		(*this._outRange).put(encodedOpts);
+		(*this._outRange).put("\"");
+	}
 }
